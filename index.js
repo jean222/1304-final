dfd8465100e9e396dcd8d1858a04bb4e0c78cbdb
jean222 <105907450+jean222@users.noreply.github.com>
2023-03-10 15:15:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ let formAnswer = [];
 let formInfo = {
     name: fullName.value,
     email: email.value,
-    
-
+    pass: pass.value
 }
+
+let inputName= document.getElementById("name");
+localStorage.setItem("name", inputName.value);
+
+
+formInfo.push(formAnswer);
+
+
 let form = document.getElementById('form');
 form.addEventListener('submit', showAnswer);
 
